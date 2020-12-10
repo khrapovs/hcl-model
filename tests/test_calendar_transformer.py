b3f00dcecf6b055a526afd0e722b8311f40a0add
Stanislav Khrapov <stanislav.khrapov@dbschenker.com>
2020-12-10 11:37:22 +0100

    Use `dates.isocalendar().week` instead of `dates.week`

--- tests/test_calendar_transformer.py
+++ tests/test_calendar_transformer.py
@@ -28,12 +28,40 @@ class TestCalendarTransformer:
         cal_transformer = CalendarTransformer()
 
         degrees_of_freedom = 3
+        df_default = cal_transformer.add_periodic_splines(
+            df=xreg, degrees_of_freedom=degrees_of_freedom
+        )
+
+        assert df_default.shape == (xreg.shape[0], degrees_of_freedom + xreg.shape[1])
+        assert np.sum(df_default.isnull()).sum() == 0
+
         df = cal_transformer.add_periodic_splines(
-            xreg, degrees_of_freedom=degrees_of_freedom
+            df=xreg, degrees_of_freedom=degrees_of_freedom, input_level="isoweek"
         )
 
         assert df.shape == (xreg.shape[0], degrees_of_freedom + xreg.shape[1])
         assert np.sum(df.isnull()).sum() == 0
+        pd.testing.assert_frame_equal(df, df_default)
+
+        df = cal_transformer.add_periodic_splines(
+            df=xreg, degrees_of_freedom=degrees_of_freedom, input_level="month"
+        )
+
+        assert df.shape == (xreg.shape[0], degrees_of_freedom + xreg.shape[1])
+        assert np.sum(df.isnull()).sum() == 0
+
+        with pytest.raises(ValueError):
+            cal_transformer.add_periodic_splines(
+                df=xreg, degrees_of_freedom=degrees_of_freedom, input_level="day"
+            )
+
+        with pytest.raises(ValueError) as error:
+            wrong_input_level = "wrong_input_level"
+            cal_transformer.add_periodic_splines(
+                df=xreg, degrees_of_freedom=degrees_of_freedom, input_level=wrong_input_level
+            )
+
+        assert wrong_input_level in str(error)
 
     def test_add_holiday_triangles(self):
         xreg = self.get_xreg_example()
