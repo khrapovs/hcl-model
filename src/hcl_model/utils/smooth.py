07193833dcbfc9117879174e62585f7860cd300e
Stanislav Khrapov <stanislav.khrapov@dbschenker.com>
2022-02-17 17:07:33 +0100

    roll back smooth_series

--- src/hcl_model/utils/smooth.py
+++ src/hcl_model/utils/smooth.py
@@ -1,15 +1,5 @@
-from typing import Union
-
-import numpy as np
 import pandas as pd
 
 
-def smooth_series(y: Union[pd.Series, np.ndarray], window: int, quantile: float, ewm_alpha: float) -> pd.Series:
-    return (
-        (pd.Series(y.flatten()) if isinstance(y, np.ndarray) else y)
-        .rolling(window=window)
-        .quantile(quantile=quantile)
-        .fillna(method="bfill")
-        .ewm(alpha=ewm_alpha)
-        .mean()
-    )
+def smooth_series(y: pd.Series, window: int, quantile: float, ewm_alpha: float) -> pd.Series:
+    return y.rolling(window=window).quantile(quantile=quantile).fillna(method="bfill").ewm(alpha=ewm_alpha).mean()
--- tests/utils/test_smooth.py
+++ tests/utils/test_smooth.py
@@ -5,20 +5,16 @@ import pytest
 from hcl_model.utils.smooth import smooth_series
 
 
-@pytest.mark.parametrize("y_type", ["series", "ndarray"])
-def test_smooth_series(y_type: str) -> None:
-    y_original_series = pd.Series(range(50))
-    y_original = y_original_series.values if y_type == "ndarray" else y_original_series
+def test_smooth_series() -> None:
+    y_original = pd.Series(range(50))
     y_smoothed = smooth_series(y=y_original, window=10, quantile=0.5, ewm_alpha=0.1)
     assert len(y_original) == len(y_smoothed)
 
-    y_original_series = pd.Series(np.zeros(100))
-    y_original = y_original_series.values if y_type == "ndarray" else y_original_series
+    y_original = pd.Series(np.zeros(100))
     y_smoothed = smooth_series(y=y_original, window=10, quantile=0.5, ewm_alpha=0.1)
-    pd.testing.assert_series_equal(y_original_series, y_smoothed)
+    pd.testing.assert_series_equal(y_original, y_smoothed)
 
-    y_original_series = pd.Series([0, 1] * 50)
-    y_original = y_original_series.values if y_type == "ndarray" else y_original_series
+    y_original = pd.Series([0, 1] * 50)
     y_smoothed = smooth_series(y=y_original, window=10, quantile=0.25, ewm_alpha=0.1)
     assert sum(y_smoothed == 0) == len(y_smoothed)
     assert sum(y_smoothed == 1) == 0
