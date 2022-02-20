4203e8aec0aa008c85c876d3f10d43ba6b0263c3
Stanislav Khrapov <stanislav.khrapov@dbschenker.com>
2022-02-20 12:21:42 +0100

    Add black to actions (#21)
    * Add black to actions
    
    * Apply black
    
    * switch off codecov comment

--- src/hcl_model/model_hcl_generic.py
+++ src/hcl_model/model_hcl_generic.py
@@ -110,7 +110,7 @@ class HandCraftedLinearModel(ModelBase):
         beta_simulated = pd.DataFrame(beta_simulated, columns=self._fit_results.params.index)
         # simulate innovations for the right hand side of the model
         innovation = np.random.normal(
-            loc=0, scale=self._fit_results.mse_resid ** 0.5, size=(num_steps, num_simulations)
+            loc=0, scale=self._fit_results.mse_resid**0.5, size=(num_steps, num_simulations)
         )
 
         # stack observed endogenous series and empty container for future simulations
