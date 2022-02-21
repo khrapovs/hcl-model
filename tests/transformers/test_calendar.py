c1c005876168adf0d9e2b9491157fb1cec7a1214
Stanislav Khrapov <stanislav.khrapov@dbschenker.com>
2022-02-21 17:22:55 +0100

    AddPeriodicSplines

--- tests/transformers/test_calendar.py
+++ tests/transformers/test_calendar.py
@@ -23,40 +23,6 @@ class TestCalendarTransformer:
             index=pd.date_range(start="2015-01-01", freq="7 D", periods=53 * 3),
         )
 
-    def test_add_periodic_splines(self):
-        xreg = self.get_xreg_example()
-        cal_transformer = CalendarTransformer()
-
-        degrees_of_freedom = 3
-        df_default = cal_transformer.add_periodic_splines(df=xreg, degrees_of_freedom=degrees_of_freedom)
-
-        assert df_default.shape == (xreg.shape[0], degrees_of_freedom + xreg.shape[1])
-        assert np.sum(df_default.isnull()).sum() == 0
-
-        df = cal_transformer.add_periodic_splines(df=xreg, degrees_of_freedom=degrees_of_freedom, input_level="isoweek")
-
-        assert df.shape == (xreg.shape[0], degrees_of_freedom + xreg.shape[1])
-        assert np.sum(df.isnull()).sum() == 0
-        pd.testing.assert_frame_equal(df, df_default)
-
-        df = cal_transformer.add_periodic_splines(df=xreg, degrees_of_freedom=degrees_of_freedom, input_level="month")
-
-        assert df.shape == (xreg.shape[0], degrees_of_freedom + xreg.shape[1])
-        assert np.sum(df.isnull()).sum() == 0
-
-        with pytest.raises(ValueError):
-            cal_transformer.add_periodic_splines(df=xreg, degrees_of_freedom=degrees_of_freedom, input_level="day")
-
-        with pytest.raises(ValueError) as error:
-            wrong_input_level = "wrong_input_level"
-            cal_transformer.add_periodic_splines(
-                df=xreg,
-                degrees_of_freedom=degrees_of_freedom,
-                input_level=wrong_input_level,
-            )
-
-        assert wrong_input_level in str(error)
-
     def test_add_holiday_triangles(self):
         xreg = self.get_xreg_example()
         cal_transformer = CalendarTransformer()
